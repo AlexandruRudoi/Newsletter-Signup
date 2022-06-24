f70b35a84e6e284997385d0a8f7b013bbf12bbcd
Alexandru Rudoi <rudoi.alexandru25@gmail.com>
2022-06-24 15:44:44 +0300

    Final

--- app.js
+++ app.js
@@ -20,19 +20,19 @@ app.get("/", function(req, res){
 
 //Setting up MailChimp
 mailchimp.setConfig({
-    //*****************************ENTER YOUR API KEY HERE******************************
-    apiKey: "5c20987cebf9121d9292044627e882cd-us5",
-    //*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
+    //*****************************API KEY******************************
+    apiKey: "93788c3dab0568c0d947aa3342790cd9-us5",
+    //*****************************API KEY PREFIX******************************
     server: "us5"
 });
 
 //As soon as the sign in button is pressed execute this
 app.post("/", function (req,res) {
-    //*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
+    //******************************************************************
     const firstName = req.body.fName;
     const lastName = req.body.lName;
     const email = req.body.email;
-    //*****************************ENTER YOU LIST ID HERE******************************
+    //*****************************LIST ID******************************
     const listId = "76675adafd";
     //Creating an object with the users data
     const subscribingUser = {
@@ -56,8 +56,7 @@ app.post("/", function (req,res) {
         console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
     }
     //Running the function and catching the errors (if any)
-    // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LESSON*************************
-    // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+    //So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
     run().catch(e => res.sendFile(__dirname + "/failure.html"));
 });
 
